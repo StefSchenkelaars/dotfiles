3f924dd13595d9cce1865e8ae1312b74378228ea
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2023-12-15 16:16:18 +0100

    Remove tmux and fix zsh on WSL

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,6 +45,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".zprofile".text = ''
+      [[ -e ~/.profile ]] && emulate sh -c 'source ~/.profile'
+    '';
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -61,7 +64,6 @@
   imports = [
     ./packages/direnv.nix
     ./packages/git.nix
-    ./packages/tmux.nix
     ./packages/zsh.nix
   ];
 
--- home-manager/packages/zsh.nix
+++ home-manager/packages/zsh.nix
@@ -4,15 +4,12 @@
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
-    shellAliases = {
-      dev = "cd ~/Documents/Development";
-    };
     oh-my-zsh = {
       enable = true;
       theme = "robbyrussell";
       plugins = [
         "git"
-	"heroku"
+        "heroku"
       ];
     };
   };
